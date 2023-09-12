78d4b2c2fe24ea771c50e5c8e6958e7b5394c243
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-12 19:43:37 -0300

    Update OperadoresLogicos4.js

--- PrepCourse/Ejemplos/Modulo 4/OperadoresLogicos4.js
+++ PrepCourse/Ejemplos/Modulo 4/OperadoresLogicos4.js
@@ -2,7 +2,6 @@ function CondicionCompleja (num) {
     if (num > 9 && num % 2 === 0 || num === 3) console.log (true);
     else console.log (false);
 };
-
 CondicionCompleja (10);
 CondicionCompleja (6);
 CondicionCompleja (3);
