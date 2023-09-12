2f9d0aed3dbe18b96e26dc226e5ee3d5fdb44334
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-12 19:43:00 -0300

    Update Bucles2.js

--- PrepCourse/Ejemplos/Modulo 4/Parte 2/Bucles2.js
+++ PrepCourse/Ejemplos/Modulo 4/Parte 2/Bucles2.js
@@ -3,11 +3,10 @@ var suma = 0
 // suma = suma + 2;
 // suma = suma + 3;
 // suma = suma + 4;
-
 // console.log (suma);
 
 for (var i = 0 ; i<5 ; i++) {
     suma = suma + i;
     console.log ("Variable de iteracion: " , i);
-}
+};
 console.log ("Variable Suma: " , suma);
